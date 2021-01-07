d2583882a84214141880dc61a19bfdbe14af4e0c
Mads Jon Nielsen <madsjon@gmail.com>
2021-01-07 16:59:24 +0100

    Made tests work.

--- index.ts
+++ index.ts
@@ -1,22 +1,53 @@
 import axios from 'axios';
 import * as papa from 'papaparse';
 import * as nunjucks from 'nunjucks';
+import * as fs from 'fs-extra';
+import * as glob from 'glob-promise';
+import * as yaml from 'js-yaml'
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+const expandEnv = (text) => {
+    return text.replace(/[$][{]?\w*[}]?/g, function (match) {
+        const sub = process.env[match.replace(/^[$][{]?/, '').replace(/[}]?$/, '')];
+        return sub || match;
+    });
+}
+
+dotenv.config();
 
 (async () => {
+    const version = '1.0.0'
+    const cwd = process.argv[2] || process.cwd();
+    const alarmaCnf = yaml.load(expandEnv(await fs.readFile(`${cwd}/alarma.yml`, 'utf8')));
 
-    const config ={
-        headers: {
+    const ymlFiles = await glob.promise(`${cwd}/**/*.yml`);
+    ymlFiles.splice(ymlFiles.indexOf(path.normalize(`${cwd}/alarma.yml`)), 1);
+
+    for (const ymlFile of ymlFiles) {
+        const yml = yaml.load(expandEnv(await fs.readFile(ymlFile, 'utf8')));
+        const njk = await fs.readFile(ymlFile.replace('.yml', '.jinja2.html'), 'utf8');
+        const sql = await fs.readFile(ymlFile.replace('.yml', '.sql'), 'utf8');
+
+        for (const elasticCnf of alarmaCnf.elastics) {
+            const elasticNjkInput = {
+                name: elasticCnf.name,
+                url: elasticCnf.url
+            }
+
+            const data = {query: `${sql}`};
+            const axiosCnf = {
+                validateStatus: () => true,
+                headers: {
+                    'Authorization': `Basic ${Buffer.from(`${elasticCnf.username}:${elasticCnf.password}`).toString('base64')}`,
+                    'User-Agent': `alarma/${version}`
+                },
+            };
+            const res = await axios.post(`${elasticCnf.url}/_sql?format=csv`, data, axiosCnf);
+            const items = (papa.parse(res.data, {header: true, skipEmptyLines: true})).data;
+            const rendered = nunjucks.renderString(njk, {items, elastic: elasticNjkInput});
+            console.log(rendered);
+
         }
-    };
-
-    try {
-        const res = await axios.post('https://elastic-mjn-org.latoyapip.org/_sql?format=csv', {
-            "query": 'SELECT host.name as hostname, system.filesystem.mount_point as mount_point, system.filesystem.device_name as device_name, ROUND(MIN(system.filesystem.used.pct) * 100,2) as used FROM metricbeat WHERE system.filesystem.mount_point NOT IN (\'/hostfs/boot\') AND system.filesystem.type = \'ext4\' AND "@timestamp" > NOW() - INTERVAL 10 MINUTE GROUP BY host.name,system.filesystem.mount_point,system.filesystem.device_name'
-        }, config);
-        const items = (papa.parse(res.data, { header: true, skipEmptyLines: true })).data;
-        const rendered = nunjucks.render('templates/filesystem-disk-usage.njk', { items, elastic: { name: 'Playtopia Prod', url: 'https://elastic-mjn-org.latoyapip.org/' } });
-        console.log(rendered);
-    } catch (e) {
-        console.log(e.toJSON());
     }
-})()
+})();
--- tests/triggers/filesystem-disk-usage.sql
+++ tests/triggers/filesystem-disk-usage.sql
@@ -1,5 +1,5 @@
 SELECT
-    host.name,
+    host.name as hostname,
     system.filesystem.mount_point as mount_point,
     ROUND(MIN(system.filesystem.used.pct) * 100,2) as used
 
@@ -8,6 +8,5 @@ FROM metricbeat
 WHERE system.filesystem.mount_point NOT IN ('/hostfs/boot')
     AND system.filesystem.type = 'ext4'
     AND "@timestamp" > NOW() - INTERVAL 10 MINUTE
-    AND used > {{ threshold.target }}
 
 GROUP BY host.name,system.filesystem.mount_point

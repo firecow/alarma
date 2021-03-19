5abbcd9515acce0bae23d91467d0eb335526fc7e
Mads Jon Nielsen <madsjon@gmail.com>
2021-03-19 20:54:21 +0100

    Moved a little closer

--- index.ts
+++ index.ts
@@ -5,9 +5,8 @@ import * as fs from 'fs-extra';
 import * as glob from 'glob-promise';
 import * as yaml from 'js-yaml'
 import * as dotenv from 'dotenv';
-import * as path from 'path';
 
-const expandEnv = (text) => {
+const expandEnv = (text: string) => {
     return text.replace(/[$][{]?\w*[}]?/g, function (match) {
         const sub = process.env[match.replace(/^[$][{]?/, '').replace(/[}]?$/, '')];
         return sub || match;
@@ -17,37 +16,49 @@ const expandEnv = (text) => {
 dotenv.config();
 
 (async () => {
-    const version = '1.0.0'
     const cwd = process.argv[2] || process.cwd();
-    const alarmaCnf = yaml.load(expandEnv(await fs.readFile(`${cwd}/alarma.yml`, 'utf8')));
+    const alarmaCnf: any = yaml.load(expandEnv(await fs.readFile(`${cwd}/alarma.yml`, 'utf8')));
+    const packageJson = JSON.parse(await fs.readFile(`package.json`, 'utf8'));
+    const version = packageJson['version'];
 
-    const ymlFiles = await glob.promise(`${cwd}/**/*.yml`);
-    ymlFiles.splice(ymlFiles.indexOf(path.normalize(`${cwd}/alarma.yml`)), 1);
+    const ymlFiles = await glob.promise(`${cwd}/triggers/**/*.yml`);
 
-    for (const ymlFile of ymlFiles) {
-        const yml = yaml.load(expandEnv(await fs.readFile(ymlFile, 'utf8')));
-        const njk = await fs.readFile(ymlFile.replace('.yml', '.jinja2.html'), 'utf8');
-        const sql = await fs.readFile(ymlFile.replace('.yml', '.sql'), 'utf8');
+    for (const ymlFilePath of ymlFiles) {
+        const trigger: any = yaml.load(expandEnv(await fs.readFile(ymlFilePath, 'utf8')));
 
-        for (const elasticCnf of alarmaCnf.elastics) {
+        for (const elasticCnf of alarmaCnf['elastics']) {
             const elasticNjkInput = {
                 id: elasticCnf.id,
                 url: elasticCnf.url
             }
 
-            const data = {query: `${sql}`};
+            const data = {"query": `${trigger['sql']}`};
             const axiosCnf = {
                 validateStatus: () => true,
                 headers: {
-                    'Authorization': `Basic ${Buffer.from(`${elasticCnf.username}:${elasticCnf.password}`).toString('base64')}`,
-                    'User-Agent': `alarma/${version}`
+                    // 'Authorization': `Basic ${Buffer.from(`${elasticCnf.username}:${elasticCnf.password}`).toString('base64')}`,
+                    'User-Agent': `alarma/${version}`,
+                    'Cookie': 'cegosso=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MTY0MzY5NzIsIm5iZiI6MTYxNTgzMjEzMiwidmVyc2lvbiI6MSwiYXV0aGVudGljYXRlZCI6dHJ1ZSwiZW1haWwiOiJtam5AY2Vnby5kayIsImRvbWFpbiI6ImNlZ28uZGsifQ.Xb5_ZrVHHe2kmoh3oTOq5-w-k9cSGjQ8XEwllolhYcrfnmWvUtDrASFiCerXIak4hs56VLO2Kb5oNXA1nDyOag',
                 },
             };
-            const res = await axios.post(`${elasticCnf.url}/_sql?format=csv`, data, axiosCnf);
-            const items = (papa.parse(res.data, {header: true, skipEmptyLines: true})).data;
-            const rendered = nunjucks.renderString(njk, {items, elastic: elasticNjkInput});
-            console.log(rendered);
+            const res = await axios.post(`${elasticCnf.url.replace(/\/$/, '')}/_sql?format=csv`, data, axiosCnf);
+            if (res.status !== 200) {
+                console.error(res.status, res.data);
+                continue;
+            }
 
+            const items = (papa.parse(res.data, {header: true, skipEmptyLines: true})).data;
+
+            const thresholds = trigger['thresholds'];
+            thresholds.sort((a: any, b: any) => b['target'] - a['target']);
+
+            for (const threshold of thresholds) {
+                const itemsExceedingThreshold = items.filter((a: any) => a[trigger['column']] > threshold['target']);
+                if (itemsExceedingThreshold.length > 0) {
+                    const rendered = nunjucks.renderString(trigger['njk'], {items: itemsExceedingThreshold, elastic: elasticNjkInput, severity: threshold['severity']});
+                    console.log(rendered);
+                }
+            }
         }
     }
 })();
